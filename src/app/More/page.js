75838bf8d8c20a67490c3e2c47af96fb6ebc670f
Mateus Collet Lacerda Machado <mateuscolletmac@gmail.com>
2024-11-11 14:52:05 -0300

    changes

--- src/app/More/page.js
+++ src/app/More/page.js
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import '../globals.css'
 
 export default function Portifolio() {
 
     return (
         <section>
+            <section className='projetos'>
+        <Link href={"https://contador-next-m.netlify.app/"}>CONTADOR |NEXT|//</Link>
+        <Link href={"https://lista-de-tarefasssss.netlify.app/"}>//LISTA DE TAREFAS |HTML|</Link>
+        </section>
             <header>
             <Link href={'/'}><button>Voltar</button></Link>
             <title>URL test</title>
--- src/app/Portifolio/page.js
+++ src/app/Portifolio/page.js
@@ -5,14 +5,16 @@ import Link from 'next/link';
 export default function Portifolio() {
 
   return (
-    <section>
-        <Link href={"./More"}><button>Testando URL</button></Link>
+    <section className='geral'>
       <header>
       <title>Meu portifólio</title>
-      <h1>Bem vindo ao meu portifolio.</h1>
+      <h1 className='principal'>Bem vindo ao meu portifolio.</h1>
       </header>
       <p>Olá, meu nome é Mateus Collet Lacerda Machado, nasci no dia 25 de janeiro de 2010 e atualmente sou estudante da ELITI, a Escola de Líderes em T.I. Desde cedo, desenvolvi um grande interesse pela tecnologia, o que me levou a buscar mais informações em programação e desenvolvimento de projetos. A ELITI tem sido um ambiente incrível para meu estudo, formação de amizades e formação de novas habilidades.
         Atualmente, estou envolvido na criação de projetos, como este, que pretendo exibir em um site para compartilhar minhas habilidades e meu progresso com a programação. Meu principal objetivo na ELITI é dominar as diversas linguagens de códigos e programação e as melhores práticas do mercado, pois acredito que essa base será essencial para alcançar algo ainda maior no futuro.</p>
+        <section className='links'>
+        <Link href={"./More"}><button>MEUS PROJETOS</button></Link>
+      </section>
       </section>
   );
 }
