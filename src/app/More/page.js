142583fc40db7e64568202c67c6ee766433c6281
Mateus Collet Lacerda Machado <mateuscolletmac@gmail.com>
2024-11-11 15:40:58 -0300

    changes

--- src/app/More/page.js
+++ src/app/More/page.js
@@ -6,8 +6,8 @@ export default function Portifolio() {
     return (
         <section>
             <section className='projetos'>
-        <Link href={"https://contador-next-m.netlify.app/"}>CONTADOR |NEXT|//</Link>
-        <Link href={"https://lista-de-tarefasssss.netlify.app/"}>//LISTA DE TAREFAS |HTML|</Link>
+        <Link href={"https://contador-next-m.netlify.app/"}>CONTADOR |NEXT|</Link>
+        <Link href={"https://lista-de-tarefasssss.netlify.app/"}>||LISTA DE TAREFAS |HTML|</Link>
         </section>
             <header>
             <Link href={'/'}><button>Voltar</button></Link>
